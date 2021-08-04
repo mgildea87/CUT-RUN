d3fe0e123bf86f59e69420cff528cbbc8936957b
Gildea <gildem01@bigpurple-ln1.cm.cluster>
2021-08-04 16:30:47 -0400

    updated error catching from cat_rename.py

--- snakemake_init.sh
+++ snakemake_init.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -l
 module load miniconda3/cpu/4.9.2
 conda activate /gpfs/data/fisherlab/conda_envs/CUT-RUN
-
-for num in {1..100}
-do
-    if ! python cat_rename.py ${num}; then
-        echo "Exiting..."
-        exit
-    fi
-done
+mkdir fastq
+if ! python cat_rename.py /gpfs/data/sequence/results/moorelab/2021-07-27/fastq/ ${1}; then
+    echo "Exiting..."
+    exit
+ fi
 
 snakemake --cluster "sbatch -J {cluster.Job_name} --mem {cluster.Mem} -c {cluster.Cores} -p {cluster.Partition} -t {cluster.Time} --output {cluster.Error}" --cluster-config cluster_config.yml --jobs 6
 snakemake --report snake_make_report.html
